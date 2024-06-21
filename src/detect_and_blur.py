9ff25c17500bbbbd81d22b9869008eff5be03580
Sotirios Papadopoulos <sotpapad@gmail.com>
2024-06-21 13:20:29 +0300

    Update detect_and_blur.py

--- src/detect_and_blur.py
+++ src/detect_and_blur.py
@@ -8,6 +8,8 @@ def blur_faces(image_path, output_path):
     image = cv2.imread(image_path)
     if image is None:
         return
+        
+    faces_num = 0
 
     # Detect faces using RetinaFace
     detections = DeepFace.extract_faces(image, detector_backend='yolov8', enforce_detection=False, align=False)
@@ -18,12 +20,16 @@ def blur_faces(image_path, output_path):
             face = image[y:y+h, x:x+w]
             face = cv2.GaussianBlur(face, (65, 65), 20)
             image[y:y+h, x:x+w] = face
+            
+            faces_num += 1
 
     cv2.imwrite(output_path, image)
-
+    return faces_num
+    
 def process_directory(directory):
     image_paths = []
     created = False
+    found_in = []
     
     for root, _, files in os.walk(directory):
         for file in files:
@@ -35,7 +41,14 @@ def process_directory(directory):
         if not created:
             created = True
         output_path = os.path.join(os.path.dirname(image_path), os.path.basename(image_path))
-        blur_faces(image_path, output_path)
+        faces_num = blur_faces(image_path, output_path)
+        if faces_num: found_in.append(image_path+"\t"+str(faces_num))
+    
+    with open('/app/images/blurred_images.txt', 'w') as f:
+        s = "Faces found in the following images (filename-number_of_faces):\n"
+        for i in found_in:
+            s += '\t' + i + '\n'    
+        f.write(s)
 
 if __name__ == '__main__':
     import sys
