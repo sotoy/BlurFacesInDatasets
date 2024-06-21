2af01cf71eb3b5452644708fb65403e6f5d40f03
Sotirios Papadopoulos <sotpapad@gmail.com>
2024-06-21 13:20:11 +0300

    Update count_faces.py

--- src/count_faces.py
+++ src/count_faces.py
@@ -19,6 +19,7 @@ def traverse_directory(directory, when):
     image_count = 0
     total_faces = 0
     image_paths = []
+    found_in = []
     for root, _, files in os.walk(directory):
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
@@ -27,12 +28,17 @@ def traverse_directory(directory, when):
     for image_path in tqdm(image_paths, desc='Counting faces'):         
         image_count += 1
         num_faces = detect_faces(image_path)
+        if num_faces > 1:
+            found_in.append(image_path+"\t"+str(num_faces))
         total_faces += num_faces
                 
     with open('/app/images/faces_'+when+'.txt', 'w') as f:
         s = "Number of images in Dataset: "+str(image_count)+'.\n'
         s += "Number of faces in Dataset "+when+" blurring: "+str(total_faces)+'.\n'
-        s += "Face frequency in Dataset: "+str((total_faces*1.0)/image_count)+' faces per image.\n'
+        s += "Face frequency in Dataset: "+str((total_faces*1.0)/image_count)+' faces per image.\n\n\n'
+        s += "Faces found in the following images (filename-number_of_faces):\n"
+        for i in found_in:
+            s += '\t' + i + '\n'
         
         f.write(s)
 
